890b55d0095be4ebd4a447c702c74eb5ec0f9f74
hanakamila28rpl <55417606+hanakamila28rpl@users.noreply.github.com>
2020-03-24 15:18:31 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    $koneksi = mysqli_connect("localhost","root","","moklet_perpustakaan");
+
+    //cek koneksi
+
+    //  if(!$koneksi){
+    //      echo "koneksi database gagal";
+    //  }else{
+    //      echo"koneksi database berhasil";
+    //  }
+?>
